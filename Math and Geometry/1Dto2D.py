9cac498fe79eca9ded7a11cd617d97f72cae5b42
Nidhishaa Ardham <166903003+nidhicode7@users.noreply.github.com>
2025-05-04 00:47:41 +0530

    Create 1Dto2D.py

---
+++ Math and Geometry/1Dto2D.py
@@ -0,0 +1,13 @@
+#Here we are converting one dimensional array to 2 dimensional array Which is a leetcode problem  2022 and what we have to do is first
+
+def convert_1d_to_2d(arr, rows, cols):
+if m*n!=len(original):
+    return []
+result=[]
+else:
+  for i in range (0,len(original),cols):
+    result.append(arr[i:i+cols])
+  return result
+    
+    
+  
