7e4f0da27b95a47b5f886a13d6543d4abda1bfc1
Nidhishaa Ardham <166903003+nidhicode7@users.noreply.github.com>
2025-05-09 23:17:33 +0530

    Update SumofPowersof3.py

--- Math and Geometry/SumofPowersof3.py
+++ Math and Geometry/SumofPowersof3.py
@@ -6,17 +6,16 @@ An integer y is a power of three if there exists an integer x such that y == 3x.
  
 
 Example 1:
-
 Input: n = 12
 Output: true
 Explanation: 12 = 31 + 32
-Example 2:
 
+Example 2:
 Input: n = 91
 Output: true
 Explanation: 91 = 30 + 32 + 34
-Example 3:
 
+Example 3:
 Input: n = 21
 Output: false
 
