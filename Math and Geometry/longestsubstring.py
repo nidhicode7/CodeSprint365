9d5914effbff0e1125b044c5f567fd1cd3abc5ce
Nidhishaa Ardham <166903003+nidhicode7@users.noreply.github.com>
2025-05-13 23:40:35 +0530

    Update longestsubstring.py

--- Math and Geometry/longestsubstring.py
+++ Math and Geometry/longestsubstring.py
@@ -18,8 +18,8 @@ Example 2:
 Input: s = "bbbbb"
 Output: 1
 Explanation: The answer is "b", with the length of 1.
-Example 3:
 
+Example 3:
 Input: s = "pwwkew"
 Output: 3
 Explanation: The answer is "wke", with the length of 3.
