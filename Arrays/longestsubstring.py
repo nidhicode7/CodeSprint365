120661ddf0074eb7cbb1ac9c527e1ace8aaab7fa
Nidhishaa Ardham <166903003+nidhicode7@users.noreply.github.com>
2025-05-22 17:21:37 +0530

    Update longestsubstring.py

--- Arrays/longestsubstring.py
+++ Arrays/longestsubstring.py
@@ -28,3 +28,18 @@ Constraints:
 0 <= s.length <= 5 * 104
 s consists of English letters, digits, symbols and spaces.....
 '''
+
+def lengthOfLongestSubstring(s: str) -> int:
+    char_set = set()
+    left = 0
+    max_len = 0
+
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+        char_set.add(s[right])
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
+
