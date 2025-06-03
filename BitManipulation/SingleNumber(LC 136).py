947ea51fa87c423f551b12f29d843f466df3fc73
Nidhishaa Ardham <166903003+nidhicode7@users.noreply.github.com>
2025-06-03 21:52:23 +0530

    Update SingleNumber(LC 136).py

--- BitManipulation/SingleNumber(LC 136).py
+++ BitManipulation/SingleNumber(LC 136).py
@@ -2,6 +2,7 @@
 #Problem: Given an array where every element appears twice except for one, find the element that appears only once.
 #Concept: XOR operation
 
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         res=0
