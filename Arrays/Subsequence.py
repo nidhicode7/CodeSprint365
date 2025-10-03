bee5c960f800cde9fc4c7e6b4fe8f2edf15d6e99
Nidhishaa Ardham <166903003+nidhicode7@users.noreply.github.com>
2025-10-03 08:34:17 +0530

    Update Subsequence.py

--- Arrays/Subsequence.py
+++ Arrays/Subsequence.py
@@ -10,7 +10,7 @@ Example 1:
 
 Input: s = "abc", t = "ahbgdc"
 Output: true
-Example 2:esuhfoegfufgfjwhdvbuihfvjeifbuk
+Example 2:esuhfoegfufgfjwhdvbuijeifbuk
 
 Input: s = "axc", t = "ahbgdc"
 Output: false
